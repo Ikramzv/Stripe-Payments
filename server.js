31663d9fe23310b6eb1c9f4928cdc9c0e624a80d
Ikram <=>
2023-06-27 20:29:56 +0400

    success,error,invalid pi pages added

--- server.js
+++ server.js
@@ -6,30 +6,56 @@ import Stripe from "stripe";
 env();
 
 const app = express();
-const stripe = Stripe()(process.env.STRIPE_PK);
+const stripe = Stripe(process.env.STRIPE_SK);
 
-app.use(express.static(path.resolve(process.cwd(), "public")));
+app.use(express.static(path.join(process.cwd(), "public")));
 app.use(express.json());
 
-const calcAmount = (items) => items.reduce((acc, item) => acc + item.price, 0);
+const calcAmount = (cart) =>
+  Object.values(cart).reduce((acc, item) => acc + item.totalPrice, 0);
 
-app.post("/create-check-out", async (req, res) => {
-  const { items } = req.body;
+app.post("/create-checkout", async (req, res) => {
+  const { cart } = req.body;
+  console.log(cart);
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: calcAmount(items),
+    amount: calcAmount(cart),
     currency: "usd",
     automatic_payment_methods: {
       enabled: true,
     },
   });
 
-  console.log(paymentIntent);
-
   res.status(201).json(paymentIntent);
 });
 
 app.get("/", (req, res) => {
   res.sendFile("index.html");
+});
+
+app.get("/error", (req, res) => {
+  return res.sendFile(
+    path.resolve(process.cwd(), "public", "pages", "error.html")
+  );
+});
+
+app.get("/success", async (req, res) => {
+  const { payment_intent_client_secret } = req.query;
+  const url = path.resolve(process.cwd(), "public", "pages");
+  if (payment_intent_client_secret == null)
+    return res.redirect(`/error?reason=No Payment Intent provided`);
+
+  res.sendFile(url + "/success.html");
+});
+
+app.get("/invalid_payment_intent", async (req, res) => {
+  return res.sendFile(
+    path.resolve(
+      process.cwd(),
+      "public",
+      "pages",
+      "invalid_payment_intent.html"
+    )
+  );
 });
 
 app.listen(3000, () => {
